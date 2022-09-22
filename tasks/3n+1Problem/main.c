940dc98538ba139e1c5b54f996d5a38181a49c2d
petrkucerak <petr@khome.cz>
2022-09-22 19:28:22 +0200

    New .gitignore rule & Add code with commented errors

--- tasks/3n+1Problem/main.c
+++ tasks/3n+1Problem/main.c
@@ -1,9 +1,13 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int function(int n) {
+  int res = 0;
   int is_stopped = 0;
   while (!is_stopped) {
-    printf("n: %d\n", n);
+    ++res;
+    /*printf("n: %d\n", n);*/
     if (n == 1) {
       is_stopped = 1;
     }
@@ -13,10 +17,29 @@ int function(int n) {
       n = n / 2;
     }
   }
-  return 0;
+  return res;
 }
 
 int main(int argc, char const *argv[]) {
-  function(22);
+  int k;
+  int j;
+  while (scanf("%d %d\n", &k, &j) == 2) {
+    if (k < 0 || k > 1000000 || j < 0 || j > 1000000) {
+      exit(EXIT_FAILURE);
+    }
+    printf("%d %d", k, j); // errors
+    if (j < k) {
+      int tmp = k;
+      k = j;
+      j = k;
+    }
+    int glob = 0;
+    for (int i = k; i <= j; ++i) {
+      int out = function(i);
+      if (out > glob)
+        glob = out;
+    }
+    printf(" %d\n", glob);
+  }
   return 0;
 }
