f23d83bedbbdd7148e4d610649771f294618238e
petrkucerak <petr@khome.cz>
2022-10-06 18:11:42 +0200

    Sort by first index

--- tasks/StackingBoxes/main.c
+++ tasks/StackingBoxes/main.c
@@ -5,18 +5,20 @@
 #define NEXT_DATA (data + k + (i * dimesnion))
 #define CURRENT_DATA (data + j + (i * dimesnion))
 
-void print_data(int *data, int boxes, int dimesnion);
-
 typedef struct box {
   int no;
   int *first_number;
 } Box;
+
+void print_data(int *data, int boxes, int dimesnion);
+void print_boxes(Box *boxes_p, int boxes, int dimesnion);
 
 int main(int argc, char const *argv[]) {
   printf("Stacking Boxes!\n");
 
   int status = 1;
   int *data = NULL;
+  Box *boxes_p = NULL;
 
   // load a data
   while (status) {
@@ -29,6 +31,7 @@ int main(int argc, char const *argv[]) {
     printf("\n");
     printf("Loaded input: %d boxes, %d dimension.\n", boxes, dimesnion);
     data = malloc(sizeof(int) * boxes * dimesnion);
+    boxes_p = malloc(sizeof(Box) * boxes);
 
     // load current data
     for (int i = 0; i < boxes; ++i) {
@@ -49,18 +52,36 @@ int main(int argc, char const *argv[]) {
           }
         }
       }
-
-      
+      boxes_p[i].no = i;
+      boxes_p[i].first_number = (data + (i * dimesnion));
     }
 
     // sorting boxes
+
+    int j = 0;
     for (int i = 0; i < boxes; ++i) {
+      for (int k = i + 1; k < boxes; ++k) {
+        if (*boxes_p[i].first_number > *boxes_p[k].first_number) {
+          Box tmp;
+          tmp.no = boxes_p[i].no;
+          tmp.first_number = boxes_p[i].first_number;
+
+          boxes_p[i].no = boxes_p[k].no;
+          boxes_p[i].first_number = boxes_p[k].first_number;
+
+          boxes_p[k].no = tmp.no;
+          boxes_p[k].first_number = tmp.first_number;
+        }
+      }
     }
 
-    print_data(data, boxes, dimesnion);
+    // print_data(data, boxes, dimesnion);
+    print_boxes(boxes_p, boxes, dimesnion);
 
     free(data);
+    free(boxes_p);
     data = NULL;
+    boxes_p = NULL;
   }
 
   return 0;
@@ -70,6 +91,15 @@ void print_data(int *data, int boxes, int dimesnion) {
   for (int i = 0; i < boxes; ++i) {
     for (int j = 0; j < dimesnion; ++j) {
       printf("%d ", *(data + j + (i * dimesnion)));
+    }
+    printf("\n");
+  }
+}
+
+void print_boxes(Box *boxes_p, int boxes, int dimesnion) {
+  for (int i = 0; i < boxes; ++i) {
+    for (int j = 0; j < dimesnion; ++j) {
+      printf("%d ", *(boxes_p[i].first_number + j));
     }
     printf("\n");
   }
